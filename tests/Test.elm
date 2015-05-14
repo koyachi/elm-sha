2cfc16cf967b1dee58682152107e57f3f0570aef
koyachi <rtk2106@gmail.com>
2015-05-14 20:54:48 +0900

    add test: testCallUpdateMultipleTimes

--- tests/Test.elm
+++ tests/Test.elm
@@ -9,6 +9,7 @@ import Graphics.Collage exposing (..)
 import Sha as S
 import Array as A
 import Maybe
+import String
 
 -- myTest = defaultTest (assertEqual 5 5)
 
@@ -41,17 +42,39 @@ testHashIsSameAsNodesCrypto =
   in
     suite title (inputs |> List.map (\i -> testInput (aGet 0 i) (aGet 1 i) (aGet 2 i)))
 
-{-
+
+inputs2 =
+  [
+   ["", "ascii", "da39a3ee5e6b4b0d3255bfef95601890afd80709"],
+   ["abc", "ascii", "a9993e364706816aba3e25717850c26c9cd0d89d"],
+   ["123", "ascii", "40bd001563085fc35165329ea1ff5c5ecbdbbeef"],
+   ["123456789abcdef123456789abcdef123456789abcdef123456789abcdef", "ascii", "d29c951b6ded5bc695a3e8268f6c069deb626c4c"],
+   ["123456789abcdef123456789abcdef123456789abcdef123456789abc", "ascii", "160ce1313a15ae4dbd2c9c45a0bca0223e12ed47"],
+   ["123456789abcdef123456789abcdef123456789abcdef123456789ab", "ascii", "f9a5c136c16964d4b2e71a55e66c821ce0cdaea0"],
+   ["0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcde", "ascii", "ef717286343f6da3f4e6f68c6de02a5148a801c4"],
+   ["0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef", "ascii", "ce4303f6b22257d9c9cf314ef1dee4707c6e1c13"],
+   ["foobarbaz", "ascii", "5f5513f8822fdbe5145af33b64d8d970dcf95c6e"]
+  ] |> List.map (\i -> A.fromList i)
+
 testCallUpdateMultipleTimes : Test
 testCallUpdateMultipleTimes =
   let title = "call update multiple times"
       testInput input encoding expected =
-        let hash = 
+        let hash = S.createHash "sha1"
+            updator s i h =
+              -- 0,2,6,14,30,62,126,254,...
+              let index2 = (i+1)*2
+              in
+                if i < (String.length s)
+                then (updator s index2 (h |> S.update (String.slice i index2 s) encoding))
+                else h
+            digest =
+              (updator input 0 hash) |> S.digest "hex"
         in
-          test ()
+          test title (assertEqual digest expected)
   in
-    suite title (inputs |> List.map (\i -> testInput (aGet 0 i) (aGet 1 i) (aGet 2 i)))
--}
+    suite title (inputs2 |> List.map (\i -> testInput (aGet 0 i) (aGet 1 i) (aGet 2 i)))
 
 main : Element
-main = runDisplay (suite "all tests" [testHashIsSameAsNodesCrypto])
+main = runDisplay (suite "all tests" [ testHashIsSameAsNodesCrypto
+                                     , testCallUpdateMultipleTimes])
--- tests/node_crypto_values.js
+++ tests/node_crypto_values.js
@@ -10,7 +10,23 @@ var inputs = [
  ['0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcde', 'ascii'],
  ['0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef', 'ascii'],
  ['foobarbaz', 'ascii']
-].forEach(function (v) {
+]
+
+console.log("# hash is the same as node's crypto");
+inputs.forEach(function (v) {
   var e = crypto.createHash('sha1').update(v[0], v[1]).digest('hex');
   console.log('["' + v[0] + '", "' + v[1] + '", "' + e + '"], ');
 });
+console.log("\n");
+
+console.log("# call update multiple times");
+inputs.forEach(function (v) {
+  var hash = crypto.createHash('sha1');
+  for (var i = 0; i < v[0].length; i = (i + 1) * 2) {
+    var s = v[0].substring(i, (i + 1) * 2);
+    hash.update(s, v[1]);
+  }
+  var e = hash.digest('hex');
+  console.log('["' + v[0] + '", "' + v[1] + '", "' + e + '"], ');
+});
+
